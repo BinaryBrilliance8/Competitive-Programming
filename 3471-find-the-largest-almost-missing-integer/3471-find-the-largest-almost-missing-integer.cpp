4fc29454ec93f3a9717248ef044cc7698d37a76e
Syed Qadeer Ahmed <148673690+BinaryBrilliance8@users.noreply.github.com>
2026-08-23 10:28:50 +0530

    Time: 0 ms (100%), Space: 29.2 MB (57.57%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> count(51, 0);
-
-        // Generate every subarray of size k
-        for (int i = 0; i <= n - k; i++) {
-            set<int> st;
-
-            // Elements inside current window
-            for (int j = i; j < i + k; j++) {
-                st.insert(nums[j]);
-            }
-
-            // Each number gets +1 for this window
-            for (int x : st) {
-                count[x]++;
+        int f[51] = {};
+        for (int val : nums) {
+            f[val]++;
+        }
+        int res = -1, n = nums.size();
+        for (int i=0; i<n; i++) {
+            if (k == n || f[nums[i]] == 1 && (k == 1 || i == 0 || i == n-1)) {
+                res = max(res, nums[i]);
             }
         }
-
-        // Find largest number appearing in exactly 1 window
-        for (int x = 50; x >= 0; x--) {
-            if (count[x] == 1) {
-                return x;
-            }
-        }
-
-        return -1;
+        return res;
     }
 };
