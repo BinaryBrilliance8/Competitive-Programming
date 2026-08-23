9dc7f0dfacd981708a438ea763eb0cbb2b068cc6
Syed Qadeer Ahmed <148673690+BinaryBrilliance8@users.noreply.github.com>
2026-08-23 20:20:53 +0530

    Time: 3 ms (66.67%), Space: 13.8 MB (97.67%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+
+        int n = num.size();
+        int half = n >> 1;
+
+        int ql = 0, qr = 0;
+        int diff = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (i < half) {
+
+                if (num[i] == '?')
+                    ql++;
+                else
+                    diff += num[i] - '0';
+
+            } else {
+
+                if (num[i] == '?')
+                    qr++;
+                else
+                    diff -= num[i] - '0';
+            }
+        }
+
+        if ((ql + qr) & 1)
+            return true;
+
+        return (diff * 2) != 9 * (qr - ql);
+    }
+};
