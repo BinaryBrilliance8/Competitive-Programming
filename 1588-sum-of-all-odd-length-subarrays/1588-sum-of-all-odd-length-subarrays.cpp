d159969d46070b94bc12345c34908fadd8d36172
Syed Qadeer Ahmed <148673690+BinaryBrilliance8@users.noreply.github.com>
2026-08-31 23:39:57 +0530

    Time: 0 ms (100%), Space: 11.2 MB (22.46%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
-
         int n = arr.size();
-
-        vector<int> prefix(n + 1, 0);
-
-        // Build prefix sum
-        for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + arr[i];
-        }
-
         int ans = 0;
 
-        // Choose starting point
-        for (int l = 0; l < n; l++) {
+        for (int i = 0; i < n; i++) {
+            int left = i + 1;
+            int right = n - i;
 
-            // Choose ending point
-            for (int r = l; r < n; r++) {
+            int count = (left * right + 1) / 2;
 
-                int length = r - l + 1;
-
-                // Only odd length
-                if (length % 2 == 1) {
-
-                    int sum = prefix[r + 1] - prefix[l];
-
-                    ans += sum;
-                }
-            }
+            ans += arr[i] * count;
         }
 
         return ans;
