fae6346007b3bf252ad2242c8e6fa10ed60fcc6a
Syed Qadeer Ahmed <148673690+BinaryBrilliance8@users.noreply.github.com>
2026-09-03 23:16:15 +0530

    Time: 0 ms (100%), Space: 8.3 MB (63.19%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int cnt = 0;
+        for (char &ch:stones) if (jewels.find(ch) != string::npos) cnt++;
+        return cnt;
+    }
+};
