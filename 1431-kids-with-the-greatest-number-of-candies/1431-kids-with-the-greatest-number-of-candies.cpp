b495f470626f6f39b13ee5dcafe6c39ecc953cc9
Syed Qadeer Ahmed <148673690+BinaryBrilliance8@users.noreply.github.com>
2026-09-03 23:19:42 +0530

    Time: 0 ms (100%), Space: 12.5 MB (26.5%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> ans;
+        int mx = *max_element(candies.begin(), candies.end());
+
+        for (int i = 0; i < candies.size(); i++) {
+            ans.push_back(candies[i] + extraCandies >= mx);
+        }
+
+        return ans;
+    }
+};
