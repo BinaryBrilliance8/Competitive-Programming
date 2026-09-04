c7caee436b864b0b1b286674b3c5d5208c7d14ea
Syed Qadeer Ahmed <148673690+BinaryBrilliance8@users.noreply.github.com>
2026-09-04 14:41:30 +0530

    Time: 0 ms (100%), Space: 31.2 MB (56.73%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> res(n);
+        res[0] = nums[0];
+        for (int i=1; i<n; i++) {
+            res[i] = max(res[i-1], nums[i]);
+        }
+        int mini = INT_MAX;
+        for (int i=n-1; i>=0; i--) {
+            mini = min(mini, nums[i]);
+            res[i] -= mini;
+        }
+        for (int i=0; i<n; i++) {
+            if (res[i] <= k) return i;
+        }
+        return -1;
+        
+    }
+};
