ec43dadd4916afb2b2ac7e12c3b9b70e5d96420e
Syed Qadeer Ahmed <148673690+BinaryBrilliance8@users.noreply.github.com>
2026-09-12 23:21:29 +0530

    Time: 16 ms (32.17%), Space: 30.9 MB (78.59%) - LeetHub

---
+++ 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+        vector<int> pos[101];
+
+        // Store positions of every value
+        for (int i = 0; i < nums.size(); i++) {
+            pos[nums[i]].push_back(i);
+        }
+
+        int ans = 0;
+
+        // Check every possible value
+        for (int x = 1; x <= 100; x++) {
+            if (pos[x].size() != 3)
+                continue;
+
+            int i1 = pos[x][0];
+            int i2 = pos[x][1];
+            int i3 = pos[x][2];
+
+            if (i2 - i1 == i3 - i2)
+                ans++;
+        }
+
+        return ans;
+    }
+};
